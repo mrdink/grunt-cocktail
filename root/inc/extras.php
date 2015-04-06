8358d94e89e34efcedad48f54a17b5ef2d5b258e
Justin Peacock <justin@sixteenbit.com>
2015-04-06 10:27:05 -0400

    initial commit

---
+++ root/inc/extras.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates
+ *
+ * Eventually, some of the functionality here could be replaced by core features
+ *
+ * @package {%= title %}
+ */
+
+/*
+ * Add responsive container to embeds
+ */
+function {%= prefix %}_embed_oembed_class($html, $url, $attr, $post_id) {
+  return '<div class="embed-container">' . $html . '</div>';
+}
+add_filter('embed_oembed_html', '{%= prefix %}_embed_oembed_class', 99, 4);
+
+/*
+ * Unregister Core Widgets
+ */
+function {%= prefix %}_default_widgets() {
+	unregister_widget('WP_Widget_Pages');
+	unregister_widget('WP_Widget_Calendar');
+	unregister_widget('WP_Widget_Archives');
+	unregister_widget('WP_Widget_Links');
+	unregister_widget('WP_Widget_Meta');
+	unregister_widget('WP_Widget_Search');
+	unregister_widget('WP_Widget_Text');
+	unregister_widget('WP_Widget_Categories');
+	unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_Recent_Comments');
+	unregister_widget('WP_Widget_RSS');
+	unregister_widget('WP_Widget_Tag_Cloud');
+	unregister_widget('WP_Nav_Menu_Widget');
+}
+add_action('widgets_init', '{%= prefix %}_default_widgets', 11);
+
+/*
+ * Callback function to filter the MCE settings
+ */
+function {%= prefix %}_mce_buttons($buttons) {
+	array_unshift($buttons, 'styleselect');
+	return $buttons;
+}
+add_filter('mce_buttons_2', '{%= prefix %}_mce_buttons');
+
+function {%= prefix %}_insert_formats( $init_array ) {
+	$style_formats = array(
+		array(
+			'title' => 'Primary Button',
+			'block' => 'a',
+			'classes' => 'button',
+			'wrapper' => true,
+		),
+		array(
+			'title' => 'Secondary Button',
+			'block' => 'a',
+			'classes' => 'secondary button',
+			'wrapper' => true,
+		)
+	);
+	$init_array['style_formats'] = json_encode( $style_formats );
+
+	return $init_array;
+}
+add_filter( 'tiny_mce_before_init', '{%= prefix %}_insert_formats' );
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function {%= prefix %}_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', '{%= prefix %}_body_classes' );
+
+if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
+	/**
+	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
+	 *
+	 * @param string $title Default title text for current view.
+	 * @param string $sep Optional separator.
+	 * @return string The filtered title.
+	 */
+	function {%= prefix %}_wp_title( $title, $sep ) {
+		if ( is_feed() ) {
+			return $title;
+		}
+
+		global $page, $paged;
+
+		// Add the blog name
+		$title .= get_bloginfo( 'name', 'display' );
+
+		// Add the blog description for the home/front page.
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) ) {
+			$title .= " $sep $site_description";
+		}
+
+		// Add a page number if necessary:
+		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+			$title .= " $sep " . sprintf( __( 'Page %s', '{%= prefix %}' ), max( $paged, $page ) );
+		}
+
+		return $title;
+	}
+	add_filter( 'wp_title', '{%= prefix %}_wp_title', 10, 2 );
+
+	/**
+	 * Title shim for sites older than WordPress 4.1.
+	 *
+	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
+	 * @todo Remove this function when WordPress 4.3 is released.
+	 */
+	function {%= prefix %}_render_title() {
+		?>
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php
+	}
+	add_action( 'wp_head', '{%= prefix %}_render_title' );
+endif;
---
+++ template.js
@@ -0,0 +1,110 @@
+/**
+ * grunt-wp-cocktail
+ * https://github.com/mrdink/grunt-wp-cocktail
+ *
+ * Copyright (c) 2015 Justin Peacock, byjust.in
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+// Basic template description
+exports.description = 'Create a WordPress theme from Cocktail.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = '';
+
+// Template-specific notes to be displayed after the question prompts.
+exports.after = '';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template
+exports.template = function( grunt, init, done ) {
+	init.process( {}, [
+		// Prompt for these values.
+		{
+			name   : 'title',
+			message: 'Theme Name',
+			default: 'Cocktail'
+		},
+		{
+			name   : 'prefix',
+			message: 'PHP function prefix (alpha and underscore characters only)',
+			default: 'cocktail'
+		},
+		init.prompt( 'version', '0.1.0' ),
+		init.prompt( 'description', 'Cocktail is a starting point. Use Bourbon, Bitters, and Neat to build your next WordPress project.' ),
+		{
+			name   : 'theme_url',
+			message: 'Theme URL',
+			default: 'https://github.com/mrdink/wp-cocktail'
+		},
+		init.prompt( 'author_name', 'Name' ),
+		init.prompt( 'author_email', 'you@email.com' ),
+		{
+			name   : 'author_url',
+			message: 'Author URL',
+			default: 'https://github.com/mrdink'
+		},
+	], function( err, props ) {
+		props.engines = {
+	    "node": ">= 0.10.0"
+		};
+		props.version = props.version;
+		props.devDependencies = {
+			"grunt": "^0.4.5",
+			"grunt-autoprefixer": "^2.0.0",
+			"grunt-contrib-clean": "^0.6.0",
+			"grunt-contrib-compress": "^0.13.0",
+			"grunt-contrib-concat": "~0.5.0",
+			"grunt-contrib-copy": "^0.8.0",
+			"grunt-contrib-cssmin": "^0.12.2",
+			"grunt-contrib-imagemin": "^0.9.4",
+			"grunt-contrib-jshint": "^0.11.1",
+			"grunt-contrib-uglify": "^0.8.1",
+			"grunt-contrib-watch": "~0.6.1",
+			"grunt-csscomb": "^3.0.0",
+			"grunt-cssjanus": "^0.2.4",
+			"grunt-modernizr": "^0.6.0",
+			"grunt-notify": "^0.4.1",
+			"grunt-rsync": "^0.6.2",
+			"grunt-sass": "^0.18.1",
+			"grunt-version-check": "^0.2.1",
+			"grunt-wp-assets": "~0.2.6",
+			"grunt-wp-i18n": "^0.5.1",
+			"load-grunt-config": "^0.16.0",
+			"load-grunt-tasks": "^3.0.0",
+			"node-sass": "^2.0.1",
+			"time-grunt": "^1.0.0",
+			"versioncheck": "^2.1.1"
+		};
+
+		// Sanitize names where we need to for PHP/JS
+		props.name = props.title.replace( /\s+/g, '-' ).toLowerCase();
+		// Development prefix (i.e. to prefix PHP function names, variables)
+		props.prefix = props.prefix.replace('/[^a-z_]/i', '').toLowerCase();
+		// Development prefix in all caps (e.g. for constants)
+		props.prefix_caps = props.prefix.toUpperCase();
+		// An additional value, safe to use as a JavaScript identifier.
+		props.js_safe_name = props.name.replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
+		// An additional value that won't conflict with NodeUnit unit tests.
+		props.js_test_safe_name = props.js_safe_name === 'test' ? 'myTest' : props.js_safe_name;
+		props.js_safe_name_caps = props.js_safe_name.toUpperCase();
+
+		// Files to copy and process
+		var files = init.filesToCopy( props );
+
+		console.log( files );
+
+		// Actually copy and process files
+		init.copyAndProcess( files, props, {noProcess: ['*.png','assets/img/*','assets/ico/*']} );
+
+		// Generate package.json file
+		init.writePackageJSON( 'package.json', props );
+
+		// Done!
+		done();
+	});
+};
